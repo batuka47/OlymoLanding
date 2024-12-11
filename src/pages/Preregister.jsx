2e8a92f1cfcca220280fafc22aa98184285a1460
SaagiiSG <saranochir.s@gmail.com>
2024-12-11 10:03:10 +0800

    lil

--- src/pages/Preregister.jsx
+++ src/pages/Preregister.jsx
@@ -9,7 +9,7 @@ const Preregister = () => {
             <input type="email" placeholder='enter your email' className='outline-none border-[1.5px] h-14 bg-transparent border-[#121212] p-3 px-4 rounded-2xl'/>
             <button className='h-14 p-3 px-5 ml-2 rounded-2xl bg-[#121212] text-[#faf9f6] active:scale-125'>Register</button>
         </div>
-        <img src={backdrop} alt="" className='rotate-180 absolute bottom-0'/>
+        <img src={backdrop} alt="" className='hidden md:block rotate-180 absolute bottom-0'/>
     </main>
   )
 }
